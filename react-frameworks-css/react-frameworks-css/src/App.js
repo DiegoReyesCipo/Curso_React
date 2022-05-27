cb81e415494abbb81c4de9cca1076e30890a581e
DiegoReyesCipo <diegoreyescipo@gmail.com>
2022-05-27 11:34:43 -0500

    upload_Frameworks

---
+++ react-frameworks-css/react-frameworks-css/src/App.js
@@ -0,0 +1,18 @@
+// import Boostrap from './components/Boostrap';
+// import Bulma from './components/Bulma';
+import React from 'react';
+import ReactBoostrap from './components/ReactBoostrap';
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks Css con React </h1>
+      {/* <Boostrap/> */}
+      {/* <Bulma/> */}
+      <ReactBoostrap/>
+       
+    </div> 
+  );
+}
+
+export default App;
